019c1a3431ebe841fe3cb7b71631ba88185dcb6f
volkangurenekli <tcsgvolkan@gmail.com>
2020-04-03 22:49:15 +0300

    added svg components

--- src/view/SearchView.js
+++ src/view/SearchView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Button} from 'react-native';
 import Box from '../components/box';
 import BoxCenter from '../components/box-center';
-
+import {Bookmark, Send} from '../components/icons/';
 function SearchView({navigation}) {
   return (
     <BoxCenter>
@@ -11,6 +11,8 @@ function SearchView({navigation}) {
         onPress={() => navigation.navigate('Detail')}
       />
       <Box size={20} bg="blue" mt={20} />
+      <Bookmark />
+      <Send />
     </BoxCenter>
   );
 }
